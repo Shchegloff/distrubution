8cd7fd6e782ca112473a5c1e42e7198a3bf3e2c4
Oleg <69995970+Shchegloff@users.noreply.github.com>
2020-09-20 18:20:54 +1000

    Задание 10 - смысловые запросы (CRUD) к БД через интерфейс ORM

--- 10 CRUD.py
+++ 10 CRUD.py
@@ -14,7 +14,7 @@ class Students(Model):
     id = PrimaryKeyField(null=False)
     fio_nm = CharField(max_length=500)
     school_nm = CharField(max_length=100)
-    corpse_no = IntegerField()
+    house_no = IntegerField()
 
     class Meta:
         database = db
@@ -22,19 +22,19 @@ class Students(Model):
 
 
 # добавление студента школы биомедицины, проживающего в корпусе 11
-st = Students(fio_nm='Щеглов Б.О.', school_nm='Школа биомедицины', corpse_no=11)
+st = Students(fio_nm='Щеглов Б.О.', school_nm='Школа биомедицины', house_no=11)
 st.save()
 
 # выбор и отображение студентов школы экономики и менеджмента и корпусов проживания
-for student in Students.select(Students.fio_nm, Students.corpse_no).\
+for student in Students.select(Students.fio_nm, Students.house_no).\
         where(Students.school_nm == 'Школа экономики и менеджмента'):
-    print(student.fio_nm + ' - ' + str(student.corpse_no))
+    print(student.fio_nm + ' - ' + str(student.house_no))
 
 # изменение корпуса студента Белова А.А. на 2й корпус
 student = Students.select().where(Students.fio_nm == 'Белов А.А.').get()
-student.corpse_no = 2
+student.house_no = 2
 student.save()
 
 # удаление записей по студентам, проживающим в 1-м корпусе
-query = Students.delete().where(Students.corpse_no == 1)
+query = Students.delete().where(Students.house_no == 1)
 n = query.execute()
